bda3c0007dd498cd73f71d509368bef4fc9a9507
Simen Bekkhus <sbekkhus91@gmail.com>
2016-08-11 15:06:33 +0200

    Update Ava plugin

--- packages/eslint-config-simenb-ava/index.js
+++ packages/eslint-config-simenb-ava/index.js
@@ -5,6 +5,6 @@ module.exports = {
   rules: {
     'arrow-parens': 'off',
     'babel/arrow-parens': ['error', 'as-needed'],
-     // 'ava/prefer-power-assert': 'error', disable until https://github.com/avajs/eslint-plugin-ava/pull/118
+    'ava/prefer-power-assert': 'error',
   },
 };
