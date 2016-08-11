f49e1e03b6a916b90600ea84177ecd53e37ef249
Simen Bekkhus <sbekkhus91@gmail.com>
2016-08-11 15:27:53 +0200

    (flow): add excludeArrowFunctions and disable unreleased rule

--- packages/eslint-config-simenb-flow/index.js
+++ packages/eslint-config-simenb-flow/index.js
@@ -11,13 +11,13 @@ module.exports = {
     'arrow-parens': 'off',
     'babel/arrow-parens': ['error', 'as-needed'],
     'flowtype/define-flow-type': 'error',
-    'flowtype/require-parameter-type': 'error',
-    'flowtype/require-return-type': 'error',
+    'flowtype/require-parameter-type': ['error', { excludeArrowFunctions: true }],
+    'flowtype/require-return-type': ['error', { excludeArrowFunctions: true }],
     'flowtype/require-valid-file-annotation': ['warn', 'always'],
     'flowtype/space-after-type-colon': 'error',
     'flowtype/space-before-type-colon': 'error',
     'flowtype/type-id-match': 'error',
     'flowtype/use-flow-type': 'error',
-    'flowtype/valid-syntax': 'error',
+    'flowtype/valid-syntax': 'off', // disable until 2.6.2 is released
   },
 };
