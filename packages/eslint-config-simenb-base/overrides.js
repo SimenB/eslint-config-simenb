c6c3ec1128b9326468157682b16e33b3a7d4de87
Simen Bekkhus <sbekkhus91@gmail.com>
2016-09-10 10:14:06 +0200

    Update base linting

--- packages/eslint-config-simenb-base/overrides.js
+++ packages/eslint-config-simenb-base/overrides.js
@@ -3,5 +3,6 @@ module.exports = {
     'max-len': ['error', 140],
     'linebreak-style': 'off',
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+    'no-plusplus': 'off',
   },
 };
