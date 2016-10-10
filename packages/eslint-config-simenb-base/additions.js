5d0d24d3aa2a11f319d6cde15c0acebbd81a3a1c
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-11 00:41:17 +0200

    Add `no-warning-comment`

--- packages/eslint-config-simenb-base/additions.js
+++ packages/eslint-config-simenb-base/additions.js
@@ -9,6 +9,7 @@ module.exports = {
     'line-comment-position': 'off',
     'lines-around-directive': 'error',
     'no-restricted-properties': 'off', // I can't think of any
+    'no-warning-comments': ['warn', { terms: ['todo', 'fixme', '@todo', '@fixme'], location: 'start' }],
     'prefer-numeric-literals': 'error',
   },
 };
