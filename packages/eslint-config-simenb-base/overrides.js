d29455a9b6e3f50da741819b68b6188b5c9def53
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-15 16:54:36 +0200

    Allow long strings and URLs

--- packages/eslint-config-simenb-base/overrides.js
+++ packages/eslint-config-simenb-base/overrides.js
@@ -1,6 +1,11 @@
 module.exports = {
   rules: {
-    'max-len': ['error', 140],
+    'max-len': ['error', 140, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+    }],
     'linebreak-style': 'off',
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'no-plusplus': 'off',
