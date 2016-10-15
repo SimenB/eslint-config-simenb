22be6daa046830c43b10c3698534c5d6ed5c8504
Simen Bekkhus <sbekkhus91@gmail.com>
2016-10-15 16:36:03 +0200

    Allow dev deps in tests

--- packages/eslint-config-simenb-base/overrides.js
+++ packages/eslint-config-simenb-base/overrides.js
@@ -4,5 +4,9 @@ module.exports = {
     'linebreak-style': 'off',
     'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
     'no-plusplus': 'off',
+    'import/no-extraneous-dependencies': ['error', {
+      devDependencies: ['**/test.js', '**/spec.js', '**/*.test.js', '**/*.spec.js'],
+      optionalDependencies: false,
+    }],
   },
 };
