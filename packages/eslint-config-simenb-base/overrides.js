770999cec5fe25965f49d200f9a86c52b5842f35
Simen Bekkhus <sbekkhus91@gmail.com>
2016-11-07 08:02:15 +0000

    Udate base airbnb dep

--- packages/eslint-config-simenb-base/overrides.js
+++ packages/eslint-config-simenb-base/overrides.js
@@ -3,6 +3,7 @@ module.exports = {
     'max-len': ['error', 140, 2, {
       ignoreUrls: true,
       ignoreComments: false,
+      ignoreRegExpLiterals: true,
       ignoreStrings: true,
       ignoreTemplateLiterals: true,
     }],
