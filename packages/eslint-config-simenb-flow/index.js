c8599dfaa14bb994098302a2be7d22c6713d30b0
Simen Bekkhus <sbekkhus91@gmail.com>
2016-12-16 17:03:32 +0100

    Update flowtype config

--- packages/eslint-config-simenb-flow/index.js
+++ packages/eslint-config-simenb-flow/index.js
@@ -2,12 +2,11 @@ module.exports = {
   extends: 'plugin:flowtype/recommended',
   plugins: ['flowtype', 'babel'],
   rules: {
-    'arrow-parens': 'off',
-    'babel/arrow-parens': ['error', 'as-needed'],
     'flowtype/define-flow-type': 'error',
     'flowtype/delimiter-dangle': ['error', 'always-multiline'],
     'flowtype/object-type-delimiter': 'error',
     'flowtype/no-dupe-keys': 'error',
+    'flowtype/no-primitive-constructor-types': 'error',
     'flowtype/no-weak-types': 'warn',
     'flowtype/require-parameter-type': ['error', { excludeArrowFunctions: true }],
     'flowtype/require-return-type': ['error', { excludeArrowFunctions: true }],
