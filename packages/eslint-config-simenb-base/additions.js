1be2cfefa1bd5305a6e603d4669c10d9f4d273f7
Simen Bekkhus <sbekkhus91@gmail.com>
2017-07-26 10:00:05 +0200

    Remove todo

--- packages/eslint-config-simenb-base/additions.js
+++ packages/eslint-config-simenb-base/additions.js
@@ -1,11 +1,6 @@
 module.exports = {
   rules: {
-    'line-comment-position': 'off',
-    'lines-around-directive': 'error',
     'no-restricted-properties': 'off', // I can't think of any
     'no-warning-comments': ['warn', { terms: ['todo', 'fixme', '@todo', '@fixme'], location: 'start' }],
-    'prefer-numeric-literals': 'error',
-    // TODO: Remove when airbnb has a release
-    'no-compare-neg-zero': 'off',
   },
 };
--- packages/eslint-config-simenb-base/legacy-additions.js
+++ packages/eslint-config-simenb-base/legacy-additions.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: ['prettier'],
   rules: {
-    'prefer-numeric-literals': 'off',
     'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true, printWidth: 120 }],
   },
 };
