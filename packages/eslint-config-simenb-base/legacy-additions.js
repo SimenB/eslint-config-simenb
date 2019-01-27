66a3df624425c62e8d2b1dfc602d345e6e38d556
Simen Bekkhus <sbekkhus91@gmail.com>
2019-01-27 13:18:02 +0100

    fix: use all rules in base config

--- packages/eslint-config-simenb-base/legacy-additions.js
+++ packages/eslint-config-simenb-base/legacy-additions.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: ['prettier'],
   rules: {
+    'no-useless-catch': 'error',
+    'import/no-named-export': 'off',
     'prettier/prettier': ['error', { trailingComma: 'es5', singleQuote: true }],
   },
 };
