bc023365b2a69cc70d0f4741da73dae912595dc2
Simen Bekkhus <sbekkhus91@gmail.com>
2022-03-07 13:00:16 +0100

    fix: update eslint and activate two new rules

--- packages/eslint-config-simenb-base/additions.js
+++ packages/eslint-config-simenb-base/additions.js
@@ -1,10 +1,12 @@
 module.exports = {
   rules: {
+    'no-nonoctal-decimal-escape': 'error',
     'no-restricted-properties': 'off', // I can't think of any
     'no-warning-comments': [
       'warn',
       { terms: ['todo', 'fixme', '@todo', '@fixme'], location: 'start' },
     ],
+    'no-unsafe-optional-chaining': 'error',
     'import/no-relative-packages': 'error',
     'import/no-import-module-exports': 'error',
   },
--- packages/eslint-config-simenb-base/index.js
+++ packages/eslint-config-simenb-base/index.js
@@ -1,10 +1,12 @@
 module.exports = {
-  extends: [
-    'eslint-config-airbnb-base',
-    'eslint-config-prettier',
-    './overrides',
-    './legacy-additions',
-    './additions',
-  ].map(require.resolve),
+  extends: ['eslint:recommended'].concat(
+    [
+      'eslint-config-airbnb-base',
+      'eslint-config-prettier',
+      './overrides',
+      './legacy-additions',
+      './additions',
+    ].map(require.resolve)
+  ),
   rules: {},
 };
